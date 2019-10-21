c5bcdbc324a28271fb9fa006989270ef0d1ea0d5
Matthew Daws <matt.daws@cantab.net>
2019-10-21 21:49:17 +0100

    Import new JPEG classes

--- pdfimage/__init__.py
+++ pdfimage/__init__.py
@@ -4,7 +4,7 @@ from . import pdf
 from . import pdf_read
 from . import pdf_write
 
-from .pdf_image import UncompressedImage, FlateImage, PNGImage, TIFFImage
+from .pdf_image import UncompressedImage, FlateImage, PNGImage, TIFFImage, JPEGImage, JPEGImageRaw
 
 from .jb2 import JBIG2Images, JBIG2CompressorToZip, JBIG2MultiImages
 
